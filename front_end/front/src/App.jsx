6ae2a6dfc4c0f11933fd9fa34bfa012af7ffba81
feliph <feliphrenaud@gmail.com>
2025-03-12 10:28:42 -0300

    commit de coreção nas rotas, assim o pagina inicial sera sempre o login e não a home

--- front_end/front/src/App.jsx
+++ front_end/front/src/App.jsx
@@ -11,8 +11,7 @@ function App() {
   return (
     <Routes>
       
-      <Route path='/' element={<Home/>} />
-      <Route path='/login' element={<Login/>} />
+      <Route path='/' element={<Login/>} />
       <Route path='/register' element={<Register/>} />
       <Route path='/dashboard' element={<Dashboard/>} />
       
