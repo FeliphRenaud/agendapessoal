de37eaf62463b6370cc096ce27afd8d1431a3312
feliph <feliphrenaud@gmail.com>
2025-03-12 10:30:13 -0300

    commit de coreção nas rotas, assim o pagina inicial sera sempre o login e não a home

--- front_end/front/src/App.jsx
+++ front_end/front/src/App.jsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Register from './pages/register/Register'
 import Dashboard from './pages/dashboard/Dashboard'
